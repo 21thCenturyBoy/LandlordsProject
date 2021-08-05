3a04f4553ae15410d2f465f8a40ff8828f0a549f
21thCenturyBoy <2524755668@qq.com>
2021-08-05 16:37:47 +0800

    添加退出房间功能，更新忽略目录

---
+++ Landlords_Server01/Landlords_Server01/Server/Hotfix/Landlords/Handler/Gate/C2G_ReturnLobby_NttHandler.cs
@@ -0,0 +1,32 @@
+﻿using ETModel;
+using System.Net;
+
+namespace ETHotfix
+{
+    [MessageHandler(AppType.Gate)]
+    public class C2G_ReturnLobby_NttHandler : AMHandler<C2G_ReturnLobby_Ntt>
+    {
+        protected override async ETTask Run(Session session, C2G_ReturnLobby_Ntt message)
+        {
+            //验证Session
+            if (!GateHelper.SignSession(session))
+            {
+                return;
+            }
+
+            User user = session.GetComponent<SessionUserComponent>().User;
+            StartConfigComponent config = Game.Scene.GetComponent<StartConfigComponent>();
+            ActorMessageSenderComponent actorProxyComponent = Game.Scene.GetComponent<ActorMessageSenderComponent>();
+
+            //通知Map服务器玩家离开房间
+            if (user.ActorID != 0)
+            {
+                ActorMessageSender actorProxy = actorProxyComponent.Get(user.ActorID);
+                actorProxy.Send(new Actor_PlayerExitRoom_G2M());
+
+                user.ActorID = 0;
+            }
+            await ETTask.CompletedTask;
+        }
+    }
+}
---
+++ Landlords_Server01/Landlords_Server01/Server/Hotfix/Landlords/Handler/Map/G2M_PlayerExitRoom_Handler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using ETModel;
+
+namespace ETHotfix
+{
+    //Gate通知Map玩家退出房间
+    [ActorMessageHandler(AppType.Map)]
+    public class G2M_PlayerExitRoom_Handler : AMActorHandler<Gamer, Actor_PlayerExitRoom_G2M>
+    {
+        protected override async ETTask Run(Gamer gamer, Actor_PlayerExitRoom_G2M message)
+        {
+            Room room;
+            if (Game.Scene.GetComponent<LandMatchComponent>().Playing.TryGetValue(gamer.UserID, out room))
+            {
+                gamer.isOffline = true;
+                //玩家断开添加自动出牌组件
+                //...
+            }
+            else //玩家不在游戏中 处于待机状态
+            {
+                Game.Scene.GetComponent<LandMatchComponent>().Waiting.TryGetValue(gamer.UserID, out room);
+                //房间移除玩家
+                room.Remove(gamer.UserID);
+                Game.Scene.GetComponent<LandMatchComponent>().Waiting.Remove(gamer.UserID);
+                //消息广播给其他人
+                room.Broadcast(new Actor_GamerExitRoom_Ntt() { UserID = gamer.UserID });
+                gamer.Dispose();
+            }
+            await ETTask.CompletedTask;
+        }
+    }
+}
